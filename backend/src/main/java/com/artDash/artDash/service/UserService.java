906704e061f5a034ad7933d7469030cc3f80b4e3
Rikko <bulletbill51@yahoo.com>
2026-02-04 14:05:23 -0800

    User Service file created.

---
+++ backend/src/main/java/com/artDash/artDash/service/UserService.java
@@ -0,0 +1,28 @@
+package com.artDash.artDash.service;
+
+import com.artDash.artDash.dto.UserDTO;
+import com.artDash.artDash.model.User;
+import com.artDash.artDash.repository.UserRepository;
+
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    public UserDTO getUserById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        return new UserDTO(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getRole(),
+                user.getBio(),
+                user.getProfilePicUrl()
+        );
+    }
+}
