8c11ac6fc16bbc6139b9802a5bd1c5b96395822c
Rikko <bulletbill51@yahoo.com>
2026-02-06 11:23:10 -0800

    Created reusable resource not found exception instead of specific exception.

--- backend/src/main/java/com/artDash/artDash/exceptions/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.artDash.artDash.exceptions;
-
-import org.springframework.http.HttpStatus;
-
-public class UserNotFoundException extends ApiException {
-    public UserNotFoundException(Long id) {
-        super(HttpStatus.NOT_FOUND, "User with id " + id + " not found");
-    }
-}
